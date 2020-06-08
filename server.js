ed4debaa47e06f7e17cfc7d05a8c4a7774e093db
jaykukadiya99 <jaykukadiya99@gmail.com>
2020-06-08 17:40:25 +0530

    dotnvfile

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express()
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Starting server at ${port}`);
+});
 
 app.get("/", (req, res) => {
     res.send("Hello Jk");
-})
-
-app.listen(3000);
+});
